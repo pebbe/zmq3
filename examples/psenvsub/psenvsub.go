dec659ac9e6e51af2aa4488f7d672e1246d1018c
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2013-03-04 20:18:07 +0100

    New example: identity

--- examples/psenvsub/psenvsub.go
+++ examples/psenvsub/psenvsub.go
@@ -9,17 +9,17 @@ import (
 )
 
 func main() {
-    //  Prepare our subscriber
-    subscriber, _ := zmq.NewSocket(zmq.SUB)
+	//  Prepare our subscriber
+	subscriber, _ := zmq.NewSocket(zmq.SUB)
 	defer subscriber.Close()
-    subscriber.Connect("tcp://localhost:5563")
-    subscriber.SetSubscribe("B")
+	subscriber.Connect("tcp://localhost:5563")
+	subscriber.SetSubscribe("B")
 
-    for {
-        //  Read envelope with address
-        address, _ := subscriber.Recv(0)
-        //  Read message contents
-        contents, _ := subscriber.Recv(0)
-        fmt.Printf("[%s] %s\n", address, contents)
-    }
+	for {
+		//  Read envelope with address
+		address, _ := subscriber.Recv(0)
+		//  Read message contents
+		contents, _ := subscriber.Recv(0)
+		fmt.Printf("[%s] %s\n", address, contents)
+	}
 }
--- examples/wuproxy/wuproxy.go
+++ examples/wuproxy/wuproxy.go
@@ -7,17 +7,17 @@ import (
 	zmq "github.com/pebbe/zmq3"
 )
 
-func main () {
-    //  This is where the weather server sits
-    frontend, _ := zmq.NewSocket(zmq.XSUB)
+func main() {
+	//  This is where the weather server sits
+	frontend, _ := zmq.NewSocket(zmq.XSUB)
 	defer frontend.Close()
-    frontend.Connect("tcp://192.168.55.210:5556")
+	frontend.Connect("tcp://192.168.55.210:5556")
 
-    //  This is our public endpoint for subscribers
-    backend, _ := zmq.NewSocket(zmq.XPUB)
+	//  This is our public endpoint for subscribers
+	backend, _ := zmq.NewSocket(zmq.XPUB)
 	defer backend.Close()
-    backend.Bind("tcp://10.1.1.0:8100")
+	backend.Bind("tcp://10.1.1.0:8100")
 
-    //  Run the proxy until the user interrupts us
-    zmq.Proxy(frontend, backend, nil)
+	//  Run the proxy until the user interrupts us
+	zmq.Proxy(frontend, backend, nil)
 }
