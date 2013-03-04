deb831f7b6d12cc3f7d658030e384a1db0a6d008
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2013-03-04 17:01:58 +0100

    Changed examples/Build.sh, location of compiled examples.

--- examples/Build.sh
+++ examples/Build.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+gobin=`go env GOBIN`
+if [ "$gobin" = "" ]
+then
+    gobin=`go env GOPATH`
+    if [ "$gobin" = "" ]
+    then
+	gobin=`go env GOROOT`
+    fi
+    gobin=`echo $gobin | sed -e 's/:.*//'`
+    gobin=$gobin/bin
+fi
+
+dir=$gobin/zmq-examples
+
+echo Installing examples in $dir
+
+mkdir -p $dir
+
+cp -u */*.sh $dir
+
 dirs=''
 for i in *
 do
     if [ -d $i ]
     then
-	if [ ! -f $i/$i -o $i/$i -ot $i/$i.go ]
+	if [ ! -f $dir/$i -o $dir/$i -ot $i/$i.go ]
 	then
 	    dirs="$dirs $i"
 	fi
@@ -18,15 +38,15 @@ then
     for i in $dirs
     do
 	cd $i
-	go build .
+	go build -o $dir/$i .
 	cd ..
     done
 else
-    libs="-r `echo $libs | perl -p -e 's/-L//; s/ -L/:/g'`"
+    libs="-r `echo $libs | sed -e 's/-L//; s/  *-L/:/g'`"
     for i in $dirs
     do
 	cd $i
-	go build -ldflags="$libs" .
+	go build -ldflags="$libs" -o $dir/$i .
 	cd ..
     done
 fi
