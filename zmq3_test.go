5467ff406be50f69597dd5e59cbe9cc490d46644
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2015-12-22 18:00:44 +0100

    Replaced examples by tests

--- zmq3_test.go
+++ zmq3_test.go
@@ -5,325 +5,170 @@ import (
 
 	"errors"
 	"fmt"
-	"runtime"
+	"testing"
 )
 
 var (
-	errerr = errors.New("error")
+	err32 = errors.New("rc != 32")
 )
 
-func Example_test_version() {
+func TestVersion(t *testing.T) {
 	major, _, _ := zmq.Version()
-	fmt.Println("Version:", major)
-	// Output:
-	// Version: 3
+	if major != 3 {
+		t.Errorf("Expected major version 3, got %d", major)
+	}
 }
 
-func Example_test_connect_delay() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_connect_resolve() {
-
-	/*
-		copied from zmq4
-	*/
+func TestConnectResolve(t *testing.T) {
 
 	sock, err := zmq.NewSocket(zmq.PUB)
-	if checkErr(err) {
-		return
+	if err != nil {
+		t.Fatal("NewSocket:", err)
 	}
+	defer func() {
+		if sock != nil {
+			sock.SetLinger(0)
+			sock.Close()
+		}
+	}()
 
 	err = sock.Connect("tcp://localhost:1234")
-	checkErr(err)
+	if err != nil {
+		t.Error("sock.Connect:", err)
+	}
 
-	err = sock.Connect("tcp://localhost:invalid")
-	fmt.Println(e(err, true))
-
-	err = sock.Connect("tcp://in val id:1234")
-	fmt.Println(e(err, true))
-
-	err = sock.Connect("invalid://localhost:1234")
-	fmt.Println(e(err, true))
+	fails := []string{
+		"tcp://localhost:invalid",
+		"tcp://in val id:1234",
+		"invalid://localhost:1234",
+	}
+	for _, fail := range fails {
+		if err = sock.Connect(fail); err == nil {
+			t.Errorf("Connect %s, expected fail, got success", fail)
+		}
+	}
 
 	err = sock.Close()
-	checkErr(err)
-
-	fmt.Println("Done")
-	// Output:
-	// error
-	// error
-	// error
-	// Done
+	sock = nil
+	if err != nil {
+		t.Error("sock.Close:", err)
+	}
 }
 
-func Example_test_disconnect_inproc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_hwm() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_invalid_rep() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_last_endpoint() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_monitor() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_msg_flags() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_inproc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_ipc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_pair_tcp() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_device() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_inproc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_ipc() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_reqrep_tcp() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_router_mandatory() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_shutdown_stress() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_sub_forward() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_term_endpoint() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func Example_test_timeo() {
-
-	fmt.Println("Done")
-	// Output:
-	// Done
-}
-
-func bounce(server, client *zmq.Socket) {
+func bounce(server, client *zmq.Socket) (msg string, err error) {
 
 	content := "12345678ABCDEFGH12345678abcdefgh"
 
 	//  Send message from client to server
-	rc, err := client.Send(content, zmq.SNDMORE)
-	if checkErr(err) {
-		return
+	rc, err := client.Send(content, zmq.SNDMORE|zmq.DONTWAIT)
+	if err != nil {
+		return "client.Send SNDMORE|DONTWAIT:", err
 	}
 	if rc != 32 {
-		checkErr(errors.New("rc != 32"))
+		return "client.Send SNDMORE|DONTWAIT:", err32
 	}
 
-	rc, err = client.Send(content, 0)
-	if checkErr(err) {
-		return
+	rc, err = client.Send(content, zmq.DONTWAIT)
+	if err != nil {
+		return "client.Send DONTWAIT:", err
 	}
 	if rc != 32 {
-		checkErr(errors.New("rc != 32"))
-	}
-
-	//  Receive message at server side
-	msg, err := server.Recv(0)
-	if checkErr(err) {
-		return
-	}
-
-	//  Check that message is still the same
-	if msg != content {
-		checkErr(errors.New(fmt.Sprintf("%q != %q", msg, content)))
-	}
-
-	rcvmore, err := server.GetRcvmore()
-	if checkErr(err) {
-		return
-	}
-	if !rcvmore {
-		checkErr(errors.New(fmt.Sprint("rcvmore ==", rcvmore)))
-		return
+		return "client.Send DONTWAIT:", err32
 	}
 
 	//  Receive message at server side
 	msg, err = server.Recv(0)
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "server.Recv 1:", err
 	}
 
 	//  Check that message is still the same
 	if msg != content {
-		checkErr(errors.New(fmt.Sprintf("%q != %q", msg, content)))
+		return "server.Recv 1:", errors.New(fmt.Sprintf("%q != %q", msg, content))
+	}
+
+	rcvmore, err := server.GetRcvmore()
+	if err != nil {
+		return "server.GetRcvmore 1:", err
+	}
+	if !rcvmore {
+		return "server.GetRcvmore 1:", errors.New(fmt.Sprint("rcvmore ==", rcvmore))
+	}
+
+	//  Receive message at server side
+	msg, err = server.Recv(0)
+	if err != nil {
+		return "server.Recv 2:", err
+	}
+
+	//  Check that message is still the same
+	if msg != content {
+		return "server.Recv 2:", errors.New(fmt.Sprintf("%q != %q", msg, content))
 	}
 
 	rcvmore, err = server.GetRcvmore()
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "server.GetRcvmore 2:", err
 	}
 	if rcvmore {
-		checkErr(errors.New(fmt.Sprint("rcvmore == ", rcvmore)))
-		return
+		return "server.GetRcvmore 2:", errors.New(fmt.Sprint("rcvmore == ", rcvmore))
 	}
 
 	// The same, from server back to client
 
 	//  Send message from server to client
 	rc, err = server.Send(content, zmq.SNDMORE)
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "server.Send SNDMORE:", err
 	}
 	if rc != 32 {
-		checkErr(errors.New("rc != 32"))
+		return "server.Send SNDMORE:", err32
 	}
 
 	rc, err = server.Send(content, 0)
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "server.Send 0:", err
 	}
 	if rc != 32 {
-		checkErr(errors.New("rc != 32"))
+		return "server.Send 0:", err32
 	}
 
 	//  Receive message at client side
 	msg, err = client.Recv(0)
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "client.Recv 1:", err
 	}
 
 	//  Check that message is still the same
 	if msg != content {
-		checkErr(errors.New(fmt.Sprintf("%q != %q", msg, content)))
+		return "client.Recv 1:", errors.New(fmt.Sprintf("%q != %q", msg, content))
 	}
 
 	rcvmore, err = client.GetRcvmore()
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "client.GetRcvmore 1:", err
 	}
 	if !rcvmore {
-		checkErr(errors.New(fmt.Sprint("rcvmore ==", rcvmore)))
-		return
+		return "client.GetRcvmore 1:", errors.New(fmt.Sprint("rcvmore ==", rcvmore))
 	}
 
 	//  Receive message at client side
 	msg, err = client.Recv(0)
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "client.Recv 2:", err
 	}
 
 	//  Check that message is still the same
 	if msg != content {
-		checkErr(errors.New(fmt.Sprintf("%q != %q", msg, content)))
+		return "client.Recv 2:", errors.New(fmt.Sprintf("%q != %q", msg, content))
 	}
 
 	rcvmore, err = client.GetRcvmore()
-	if checkErr(err) {
-		return
+	if err != nil {
+		return "client.GetRcvmore 2:", err
 	}
 	if rcvmore {
-		checkErr(errors.New(fmt.Sprint("rcvmore == ", rcvmore)))
-		return
+		return "client.GetRcvmore 2:", errors.New(fmt.Sprint("rcvmore == ", rcvmore))
 	}
-
-}
-
-func checkErr(err error) bool {
-	if err != nil {
-		_, filename, lineno, ok := runtime.Caller(1)
-		if ok {
-			fmt.Printf("%v:%v: %v\n", filename, lineno, err)
-		} else {
-			fmt.Println(err)
-		}
-		return true
-	}
-	return false
-}
-
-func e(err error, willfail bool) error {
-	if err == nil || willfail == false {
-		return err
-	}
-	return errerr
+	return "OK", nil
 }
