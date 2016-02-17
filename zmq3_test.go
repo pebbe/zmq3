e0b48db71ec3db9e6dba53cd6fbeed35ee224018
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-02-17 20:32:08 +0100

    Fix in TestPoller, closing sockets

--- zmq3_test.go
+++ zmq3_test.go
@@ -61,7 +61,7 @@ func TestPoller(t *testing.T) {
 	var sb, sc *zmq.Socket
 
 	defer func() {
-		for _, s := range []*zmq.Socket{sb, sb} {
+		for _, s := range []*zmq.Socket{sb, sc} {
 			if s != nil {
 				s.SetLinger(0)
 				s.Close()
